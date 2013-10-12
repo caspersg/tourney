f227ff550108312b8fd8ca503f0d87b42ecbc554
naedyr <naedyr@gmail.com>
2013-10-12 17:02:59 +1100

    adding view links between models

--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,2 +1,4 @@
 class Fighter < ActiveRecord::Base
+  has_many :wins, :foreign_key => 'winner_id', :class_name => "Fight"
+  has_many :losses, :foreign_key => 'loser_id', :class_name => "Fight"
 end
